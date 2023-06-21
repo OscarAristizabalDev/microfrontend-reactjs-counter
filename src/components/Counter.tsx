05c42dd49e39d0103a06129f20c271b8f8ca65f6
OscarAristizabalDev <oscar.aristi.jaimes@gmail.com>
2023-06-21 09:52:18 -0500

    updated Counter component using StyleComponenet with emoticn

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,7 +1,15 @@
 import React, { useState } from 'react'
+import styled from '@emotion/styled';
 
 import "./Counter.css"
 import { Button } from './Button';
+
+const Success = styled.span`
+    color: lime;
+`
+const Error = styled.span`
+    color: tomato;
+`
 
 export const Counter = () => {
 
@@ -17,7 +25,14 @@ export const Counter = () => {
 
     return (
         <div className='Counter-container'>
-            <h3>Counter: {counter}</h3>
+            {/* <h3>Counter: <Success>{counter}</Success></h3> */}
+            <h3>Counter:
+                {
+                    (counter < 0)
+                        ? <Error>{counter}</Error>
+                        : <Success>{counter}</Success>
+                }
+            </h3>
             <Button text='+ 1' onClickCounter={onSumar} />
             <Button text='- 1' onClickCounter={onRestar} />
         </div>
